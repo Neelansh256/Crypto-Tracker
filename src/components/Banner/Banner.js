9d898b5ac7a0e786963171855e1917874f1b8a57
Neelansh Raghav <raghavneelansh@gmail.com>
2022-05-17 09:11:50 +0530

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -51,7 +51,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Get all the Info regarding your favorite Crypto Currency
+            Get all the info regarding your favourite cryptocurrencies.
           </Typography>
         </div>
         <Carousel />
